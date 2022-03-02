c421da690fb28bf4b963627b5cdddd5f7a3785fc
qbhy <96qbhy@gmail.com>
2022-03-02 20:51:07 +0800

    feat: 优化类序列化器实例化逻辑

--- service_provider.go
+++ service_provider.go
@@ -16,17 +16,16 @@ func (this *ServiceProvider) Register(application contracts.Application) {
 	application.Singleton("serializer", func(config contracts.Config, serialization contracts.Serialization) contracts.Serializer {
 		return serialization.Method(config.Get("serialization").(Config).Default)
 	})
-	application.Singleton("class.serializer", func() contracts.ClassSerializer {
-		return NewClassSerializer(application)
+	application.Singleton("class.serializer", func(config contracts.Config) contracts.ClassSerializer {
+		var serializer = NewClassSerializer(application)
+		for _, class := range config.Get("serialization").(Config).Class {
+			serializer.Register(class)
+		}
+		return serializer
 	})
 }
 
 func (this *ServiceProvider) Start() error {
-	this.app.Call(func(config contracts.Config, serializer contracts.ClassSerializer) {
-		for _, class := range config.Get("serialization").(Config).Class {
-			serializer.Register(class)
-		}
-	})
 	return nil
 }
 
